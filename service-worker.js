78d0db61338953e9b1bbf5eefa024fdf44325c42
Sr_Raposo <viniciusrinaldi273@gmail.com>
2025-03-03 18:29:20 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,52 @@
-const CACHE_NAME = 'pagina-especial-v1';
+const CACHE_NAME = 'perfeita-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/theme.js',
-    '/clouds.js',
-    '/stars.js',
-    '/oneko.js',
-    '/food.js',
-    '/assets/oneko.gif',
-    '/assets/onezumi.gif',
-    '/assets/icon-192x192.png',
-    '/assets/icon-512x512.png'
+    '/meubebe/',
+    '/meubebe/index.html',
+    '/meubebe/styles.css',
+    '/meubebe/script.js',
+    '/meubebe/theme.js',
+    '/meubebe/clouds.js',
+    '/meubebe/stars.js',
+    '/meubebe/oneko.js',
+    '/meubebe/food.js',
+    '/meubebe/assets/oneko.gif',
+    '/meubebe/assets/onezumi.gif',
+    '/meubebe/assets/icon-192x192.png',
+    '/meubebe/assets/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
+// Instalação
+self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then((cache) => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', event => {
+// Ativação
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 }); 
